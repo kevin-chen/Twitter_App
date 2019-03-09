7afba01725ab84191a15d02674f4a5bbda2b8b68
Kevin Chen <kc3585@nyu.edu>
2019-03-09 15:12:11 -0500

    Project Finished

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Kevin Chen on 3/9/2019.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
